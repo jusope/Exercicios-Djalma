afd8544b3a2874bb63459e8c607dd7c92b2c9628
jusope <jusope@uol.com.br>
2022-07-06 18:41:43 -0300

    Exercicio5

---
+++ Exercicio5Djalma/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio5Djalma
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int i, num;
+
+            for (i = 1; i <= 100; i++)
+            {
+                num = i % 2;
+                if (num == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
